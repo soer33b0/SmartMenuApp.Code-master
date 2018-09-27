0329430b7fc64fa86f134927ea02b6b0a3d9f8af
Jimmy Martin Henriksen <jimm1034@edu.eal.dk>
2018-09-27 10:39:39 +0200

    Binding.cs method made

---
+++ SmartMenuApp/Binding.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartMenuApp
+{
+    class Binding
+    {
+        public static string DoThis()
+        {
+            string heightString, widthString;
+            double width, height, rectAngle;
+            Console.WriteLine("Indtast første variabel");
+            heightString = Console.ReadLine();
+            height = double.Parse(heightString);
+            Console.WriteLine("Indtast anden variabel");
+            widthString = Console.ReadLine();
+            width = double.Parse(widthString);
+            rectAngle = width * height;
+            Console.WriteLine("Rektangel er lig med" + rectAngle);
+        }
+        /*Koble menuvalg fra SmartMenu 
+        til det udleverede softwarebibliotek 
+        FunctionLibrary*/
+        /* fx. tast 4 target menu 4 dosomething */
+    }
+}
