c3ed48f72f140003a95bb0596bfe076aca74c835
Søren Gjedsøe Eriksen <soer33b0@edu.eal.dk>
2018-09-27 08:35:46 +0200

    Path

--- SmartMenuLibrary/SmartMenu.cs
+++ SmartMenuLibrary/SmartMenu.cs
@@ -10,7 +10,22 @@ namespace SmartMenuLibrary
     {
         public void LoadMenu(string path)
         {
-            // Implement ....
+            int counter = 0;
+            string line;
+
+            // Read the file and display it line by line.  
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(@"C:\Users\Søren\Desktop\SmartMenuApp.Code-master\menutxt.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                System.Console.WriteLine(line);
+                counter++;
+            }
+
+            file.Close();
+            System.Console.WriteLine("There were {0} lines.", counter);
+            // Suspend the screen.  
+            System.Console.ReadLine();
         }
         public void Activate()
         {
