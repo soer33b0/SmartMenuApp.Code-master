120b54fa1cdcf51193bf1bab9121ded6e3e31946
Jimmy Martin Henriksen <jimm1034@edu.eal.dk>
2018-09-27 09:08:47 +0200

    MenuSpec.txt comment

--- SmartMenuLibrary/SmartMenu.cs
+++ SmartMenuLibrary/SmartMenu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace SmartMenuLibrary
 {
@@ -15,7 +16,8 @@ namespace SmartMenuLibrary
 
             // Read the file and display it line by line.  
             System.IO.StreamReader file =
-                new System.IO.StreamReader(@"C:\Users\Søren\Desktop\SmartMenuApp.Code-master\menutxt.txt");
+                new System.IO.StreamReader(@"C:\Users\Tatjana\Source\Repos\SmartMenuApp.Code-master\SmartMenuApp\MenuSpec.txt");
+            //Loads MenuSpec.txt
             while ((line = file.ReadLine()) != null)
             {
                 System.Console.WriteLine(line);
